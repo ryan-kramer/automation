dab1d820ee78778179b82f13596e69048a515304
Ryan Kramer <rkramer@gmail.com>
2024-04-25 14:47:06 -0500

    Create getmap.bash

---
+++ getmap.bash
@@ -0,0 +1,16 @@
+
+rm /mnt/ramdisk/FireIndex_NEW.png
+rm -fr /mnt/ramdisk/*
+rm /mnt/NDDESFireIndex.png
+cd /mnt/ramdisk
+
+#rm /root/newfire/FireIndex_NEW.png
+#cd /root/newfire
+
+#wget https://www.nd.gov/des/NDElevationLookup/FireDeclarations/FireIndex_NEW.png
+wget https://gis.des.nd.gov/NDDESFireIndex.png
+
+
+
+
+
